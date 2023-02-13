611a49afece173ae3761661145c0af745c9063ad
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2023-02-13 20:56:42 +0100

    börjat m fotnoter

--- data/staticdata.js
+++ data/staticdata.js
@@ -1,7 +1,7 @@
 const events = [
     {
         id: 3,
-        date: '2022-04-28',
+        date: '2023-04-28',
         title: 'Långfredagen',
         subtitle: 'Dagen för Jesu korsfästelse och död',
         description: 'Långfredagen är dagen för Jesu korsfästelse och död.',
@@ -9,7 +9,7 @@ const events = [
     },
     {
         id: 4,
-        date: '2022-05-01',
+        date: '2023-05-01',
         title: 'Påskdagen',
         subtitle: 'Till minne av Jesu uppståndelse från de döda',
         description: 'Påskdagen firas till minne av Jesu uppståndelse från de döda.',
@@ -17,7 +17,14 @@ const events = [
     },
     {
         id: 5,
-        date: "2022-12-09",
+        date: "2023-12-09",
+        title: "Johannes Cassianus",
+        description: "Förmedlade den östliga munktraditionens ljus till Väst",
+        link: "/synaxarium/johannes-cassianus"
+    },
+    {
+        id: 5,
+        date: "2023-02-13",
         title: "Johannes Cassianus",
         description: "Förmedlade den östliga munktraditionens ljus till Väst",
         link: "/synaxarium/johannes-cassianus"
